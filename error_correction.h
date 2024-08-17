d6b79078aca2538f33feb9bf4db07c680167f54d
nzengi <zenginureddin1@gmail.com>
2024-08-17 04:20:33 +0300

    Create error_correction.h

---
+++ error_correction.h
@@ -0,0 +1,10 @@
+#ifndef ERROR_CORRECTION_H
+#define ERROR_CORRECTION_H
+
+#include "lattice.h"
+
+// Error correction methods
+LatticeVector errorCorrection(LatticeVector vec);
+bool checkForErrors(LatticeVector vec);
+
+#endif // ERROR_CORRECTION_H
